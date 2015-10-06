490f46ea81cc65a80542f5c6e5807dc91f9dc5ac
imzig <mail.imzig@gmail.com>
2015-10-07 00:09:58 +0900

    first sample

---
+++ first/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  context: __dirname + '/src',
+  entry: { javascript: "./app.js", html: "./index.html" },
+  output: {
+    path: __dirname + '/dist',
+    filename: "app.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.html$/, loader: "file?name=[name].[ext]" }
+    ]
+  }
+}
+
